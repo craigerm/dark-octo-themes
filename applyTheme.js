77865e82d93b11fc22511a4b9dce0b04fe13b3d0
Craig <craig@linux001.(none)>
2012-04-08 07:07:46 -0400

    Now theme will stay on for new github tabs and refreshing

--- applyTheme.js
+++ applyTheme.js
@@ -1,9 +1,29 @@
 // For now we have just the one theme
 var theme = "themes/vibrantink.css";
+var isEnabled = false;
 
-function includeTheme(){
-  chrome.tabs.insertCSS(null, {file: theme});
+function eval(script){
+  chrome.tabs.executeScript(null,{ code: script });
 }
 
-chrome.browserAction.onClicked.addListener(includeTheme);
+function includeTheme(tab){
+  if(isEnabled && tab.url.match(/https:\/\/github.com/)){
+    chrome.tabs.insertCSS(tab.id, {file: theme});
+  }
+}
+
+chrome.browserAction.onClicked.addListener(function(tab){
+  isEnabled = !isEnabled;
+  includeTheme(tab);
+});
+
+chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab){
+  if(changeInfo.url || changeInfo.status === "loading"){
+    includeTheme(tab);
+  }
+});
+
+chrome.tabs.onCreated.addListener(function(tab){
+  includeTheme(tab);
+});
 
