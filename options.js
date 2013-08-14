d0e9fed878d6fc0cff15792a7d288d2f92421ed8
craigerm <craigerm@gmail.com>
2013-08-14 18:06:27 -0400

    Rework options page to use predefined theme

--- options.js
+++ options.js
@@ -1,20 +1,32 @@
-function saveButton() {
-  saveScheme(document.getElementById("css").value);
-  
-    var status = document.getElementById("status");
-    status.innerHTML = "Options Saved.";
-    setTimeout(function() {
-      status.innerHTML = "";
-    }, 750);
+function onClickSave() {
+  var theme = document.getElementById('theme').value;
+  setStatus('Theme changed to ' + theme);
+  ThemeStorage.save(theme);
 }
 
-function setSchemeEditor(scheme) {
-	var css = document.getElementById("css");
-	css.value = scheme;
+function setStatus(message) {
+  var status = document.getElementById("status");
+  status.innerHTML = message;
+  status.style.display = 'block';
+  setTimeout(function() {
+    status.innerHTML = "";
+    status.style.display = 'none';
+  }, 2000);
 }
 
+function initForm() {
+  console.log('Loading current theme %s', ThemeStorage.getCurrent());
+  document.getElementById('theme').value = ThemeStorage.getCurrent();
+  document.querySelector('#save').addEventListener('click', onClickSave);
+}
 
-document.querySelector('#save').addEventListener('click', saveButton);
+//function setSchemeEditor(scheme) {
+//  var css = document.getElementById("css");
+//  css.value = scheme;
+//}
 
-var css = document.getElementById("css");
-setSchemeEditor(currentScheme());
+
+initForm();
+
+//var css = document.getElementById("css");
+//setSchemeEditor(currentScheme());
